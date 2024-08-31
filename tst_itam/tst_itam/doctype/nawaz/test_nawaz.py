45228ab141806d637ab186ec7ccd5f0ab9be3565
tst-it-1 <kirollos@tst.ly>
2024-08-31 11:48:48 +0000

    removing test doctypes

--- tst_itam/tst_itam/doctype/nawaz/test_nawaz.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Kiro TST R&D Department and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestNawaz(FrappeTestCase):
-	pass
