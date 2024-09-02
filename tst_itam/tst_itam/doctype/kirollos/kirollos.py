70954c3c85415039f0472a1ae17c3cbcd1545300
tst-it-1 <kirollos@tst.ly>
2024-09-02 10:24:25 +0000

    Version 0.2

--- tst_itam/tst_itam/doctype/kirollos/kirollos.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Kiro TST R&D Department and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class kirollos(Document):
-	pass
