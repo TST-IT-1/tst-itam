60c819445c25451345133b06a9f624d460d913dd
tst-it-1 <kirollos@tst.ly>
2024-09-02 11:23:01 +0000

    Version 0.21 emp updates

---
+++ tst_itam/tst_itam/doctype/t_employee/test_t_employee.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Kiro TST R&D Department and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTEmployee(FrappeTestCase):
+	pass
